b132fe08c9b64152744057be821ee544f0ebecd8
Patryk Jajdyk <89976211+jajdys@users.noreply.github.com>
2022-03-29 01:26:10 +0200

    Update calendar.java

--- calendar.java
+++ calendar.java
@@ -3,13 +3,20 @@ public class Kalendarz{
     public static void main(String []args){
         Scanner scan = new Scanner(System.in);
         String[] miesionce = new String[] {"styczeń", "luty"," marzec", "kwiecien","maj","czerwiec","lipiec", "sierpiem","wrzesien", "pazdziernik","listopad","grudzien"}; //tablica dla miesiecy 
-        String[][] styczen = {//przykladowa tablica miesiąca
-	        { "==", "==", "==", "==", "==", "01", "02"},
-	        { "03", "04", "05", "06","07", "08", "09"},
-	        { "10", "11", "12", "13", "14", "15", "16"},
-	        { "17", "18", "19", "20", "21", "22", "23"},
-	        { "24", "25", "26", "27", "28", "29", "30"},
-            { "31", "==", "==", "==", "==", "==", "=="}
+        String[][] styczen = {//tablica dla miesiaca styczen
+	     	{ "==", "==", "==", "==", "==", "01", "02"},
+		{ "03", "04", "05", "06", "07", "08", "09"},
+		{ "10", "11", "12", "13", "14", "15", "16"},
+		{ "17", "18", "19", "20", "21", "22", "23"},
+		{ "24", "25", "26", "27", "28", "29", "30"},
+		{ "31", "==", "==", "==", "==", "==", "=="}
+            };
+        String[][] luty = {//tablica dla miesiaca luty
+		{ "==", "01", "02", "03", "04", "05", "06"},
+            	{ "07", "08", "09", "10", "11", "12", "13"},
+            	{ "14", "15", "16", "17", "18", "19", "20"},
+            	{ "21", "22", "23", "24", "25", "26", "27"},
+            	{ "28", "==", "==", "==", "==", "==", "=="}
             };
 
         Scanner keyboard = new Scanner(System.in);
@@ -17,7 +24,6 @@ public class Kalendarz{
         int rok = keyboard.nextInt();
         System.out.println("Wybierz podany miesiąc(1-12): ");
         int miesionc = keyboard.nextInt();
-
 
         if(miesionc==1){
             System.out.println("| Styczeń " + rok + " |");
@@ -30,11 +36,22 @@ public class Kalendarz{
                     }
                 }
                 for(int y = 1; y < 4; y++){
-                    System.out.printf("| " + styczen[y][y] + " |\t\n");
+                    System.out.printf("| " + styczen[y][0] + " |\t\n");
                 }
         }
         else if(miesionc==2){
-            
+            System.out.println("| Luty " + rok + " |");
+                for(int x = 0; x < 28; x++){
+                    if(x % 7 == 0){
+                        System.out.printf("| " + luty[0][x] + " |\t");
+                    }
+                    else{
+                        System.out.printf("| " + luty[0][x] + " |\t");
+                    }
+                }
+                for(int y = 1; y < 4; y++){
+                    System.out.printf("| " + luty[y][0] + " |\t\n");
+                }
         }
         else if(miesionc==3){
             
