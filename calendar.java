ef0491e6a2021f9e9462c8e5a81d614e2ba2599c
Patryk Jajdyk <89976211+jajdys@users.noreply.github.com>
2022-04-06 22:59:51 +0200

    Update calendar.java

--- calendar.java
+++ calendar.java
@@ -4,40 +4,42 @@ public class Kalendarz{
         Scanner scan = new Scanner(System.in);
         String[] miesionce = new String[] {"styczeń", "luty"," marzec", "kwiecien","maj","czerwiec","lipiec", "sierpiem","wrzesien", "pazdziernik","listopad","grudzien"}; //tablica dla miesiecy 
         String[][] styczen = {//tablica dla miesiaca styczen
-	     	{ "==", "==", "==", "==", "==", "01", "02"},
-		{ "03", "04", "05", "06", "07", "08", "09"},
-		{ "10", "11", "12", "13", "14", "15", "16"},
-		{ "17", "18", "19", "20", "21", "22", "23"},
-		{ "24", "25", "26", "27", "28", "29", "30"},
-		{ "31", "==", "==", "==", "==", "==", "=="}
-            };
+	{ "==", "==", "==", "==", "==", "01", "02"},
+	{ "03", "04", "05", "06", "07", "08", "09"},
+	{ "10", "11", "12", "13", "14", "15", "16"},
+	{ "17", "18", "19", "20", "21", "22", "23"},
+	{ "24", "25", "26", "27", "28", "29", "30"},
+	{ "31", "==", "==", "==", "==", "==", "=="}
+        };
         String[][] luty = {//tablica dla miesiaca luty
-		{ "==", "01", "02", "03", "04", "05", "06"},
-            	{ "07", "08", "09", "10", "11", "12", "13"},
-            	{ "14", "15", "16", "17", "18", "19", "20"},
-            	{ "21", "22", "23", "24", "25", "26", "27"},
-            	{ "28", "==", "==", "==", "==", "==", "=="}
-            };
+	{ "==", "01", "02", "03", "04", "05", "06"},
+        { "07", "08", "09", "10", "11", "12", "13"},
+        { "14", "15", "16", "17", "18", "19", "20"},
+        { "21", "22", "23", "24", "25", "26", "27"},
+        { "28", "==", "==", "==", "==", "==", "=="}
+        };
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Który rok chciałbyś zobaczyć?");
         int rok = keyboard.nextInt();
         System.out.println("Wybierz podany miesiąc(1-12): ");
         int miesionc = keyboard.nextInt();
-
+        int y = 0;
+        
         if(miesionc==1){
             System.out.println("| Styczeń " + rok + " |");
                 for(int x = 0; x < 31; x++){
-                    if(x % 7 == 0){
-                        System.out.printf("| " + styczen[0][x] + " |\t");
+                    if(x % 6 == 0){
+                        System.out.printf("| " + styczen[y][x] + " |\t");
+                        y++;
                     }
                     else{
-                        System.out.printf("| " + styczen[0][x] + " |\t");
+                        System.out.printf("| " + styczen[y][x] + " |\t");
                     }
                 }
-                for(int y = 1; y < 4; y++){
-                    System.out.printf("| " + styczen[y][0] + " |\t\n");
-                }
+                //for(int y = 1; y < 4; y++){
+                //    System.out.printf("| " + styczen[y][0] + " |\t\n");
+                //}
         }
         else if(miesionc==2){
             System.out.println("| Luty " + rok + " |");
@@ -49,9 +51,9 @@ public class Kalendarz{
                         System.out.printf("| " + luty[0][x] + " |\t");
                     }
                 }
-                for(int y = 1; y < 4; y++){
-                    System.out.printf("| " + luty[y][0] + " |\t\n");
-                }
+                //for(int y = 1; y < 4; y++){
+                //    System.out.printf("| " + luty[y][0] + " |\t\n");
+                //}
         }
         else if(miesionc==3){
             
